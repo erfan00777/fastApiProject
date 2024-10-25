d229ccdce6945e37da3b5774f39a50bba1a9f1f8
erfan00777 <erfan.00777@gmail.com>
2024-10-26 01:14:18 +0330

    change : change helper.py

--- app/helper.py
+++ app/helper.py
@@ -1,4 +1,4 @@
-from clickhouse_connect import Client
+import clickhouse_connect
 from app.config import (
     CLICKHOUSE_HOST,
     CLICKHOUSE_PORT,
@@ -10,24 +10,43 @@ from app.config import (
 
 client = None
 
-def get_client():
+def create_client():
 
     global client
     if client is None:
-        client = Client(
-            host=CLICKHOUSE_HOST,
-            port=CLICKHOUSE_PORT,
-            username=CLICKHOUSE_USER,
-            password=CLICKHOUSE_PASSWORD,
-            database=CLICKHOUSE_DATABASE,
-            pool_min_size=5,
-            pool_max_size=20
+        client = clickhouse_connect.get_client(
+                                    host=CLICKHOUSE_HOST,
+                                    port=CLICKHOUSE_PORT,
+                                    username=CLICKHOUSE_USER,
+                                    password=CLICKHOUSE_PASSWORD,
+                                    database=CLICKHOUSE_DATABASE
+                                    # pool_min_size=5,
+                                    # pool_max_size=20
         )
     return client
 
+
+def create_table():
+
+    client = create_client()
+
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS user_view (
+        user_id Int32,
+        created_at DateTime,
+        page_url String
+    ) ENGINE = ReplacingMergeTree()
+    PARTITION BY toYYYYMM(created_at)
+    ORDER BY (user_id, created_at)
+    SETTINGS index_granularity = 8192
+    """
+
+    client.command(create_table_query)
+
+
 def execute_query(query: str, params: tuple = ()):
 
-    return get_client().execute(query, params)
+    return create_client().execute(query, params)
 
 def close_client():
 
@@ -35,3 +54,4 @@ def close_client():
     if client:
         client.close()
         client = None
+
