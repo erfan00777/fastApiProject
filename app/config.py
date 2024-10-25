5fcea79c1722d7d45bc095558a31a0edab92429d
erfan00777 <erfan.00777@gmail.com>
2024-10-25 21:10:41 +0330

    change : change config.py

--- app/config.py
+++ app/config.py
@@ -3,8 +3,10 @@ import os
 
 load_dotenv()
 
-CLICKHOUSE_HOST = os.getenv("CLICKHOUSE_HOST", "localhost")
-CLICKHOUSE_PORT = os.getenv("CLICKHOUSE_PORT", "8123")
-CLICKHOUSE_USER = os.getenv("CLICKHOUSE_USER", "default")
-CLICKHOUSE_PASSWORD = os.getenv("CLICKHOUSE_PASSWORD", "your_password")
-CLICKHOUSE_DATABASE = os.getenv("CLICKHOUSE_DATABASE", "your_database")
+CLICKHOUSE_HOST = os.getenv("CLICKHOUSE_HOST", "host")
+CLICKHOUSE_PORT = os.getenv("CLICKHOUSE_PORT", "port")
+CLICKHOUSE_USER = os.getenv("CLICKHOUSE_USER", "user")
+CLICKHOUSE_PASSWORD = os.getenv("CLICKHOUSE_PASSWORD", "password")
+CLICKHOUSE_DATABASE = os.getenv("CLICKHOUSE_DATABASE", "database")
+
+
