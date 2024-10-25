b3e9555a1eb01f1e3312e7a2c4be2fd0ea11cbbf
erfan00777 <erfan.00777@gmail.com>
2024-10-25 21:20:32 +0330

    change : change models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from datetime import datetime
 
-class TrafficData(BaseModel):
+class check_schema(BaseModel):
     user_id: int
     created_at: datetime
     page_url: str
