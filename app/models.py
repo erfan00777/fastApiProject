f0dba370458f1b1e99cff9e6d9d1e536aa21d793
erfan00777 <erfan.00777@gmail.com>
2024-10-25 21:24:52 +0330

    change : change models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
 from datetime import datetime
 
-class check_schema(BaseModel):
+
+class RequestSchema(BaseModel):
     user_id: int
     created_at: datetime
     page_url: str
+
+class ResponseSchema(BaseModel):
+    url: str
+    count: int
